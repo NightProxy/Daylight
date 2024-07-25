02d3e544282e497ef87bac2bc165312fb55452d4
Dust <krithikrajan3@gmail.com>
2024-07-25 17:16:42 -0400

    Updated all components

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  daisyui: {
-    themes: ["light", "dark"],
-  },
   darkMode: ["class"],
   content: [
     './pages/**/*.{ts,tsx}',
@@ -10,6 +7,7 @@ module.exports = {
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
   ],
+  prefix: "",
   theme: {
     container: {
       center: true,
@@ -61,24 +59,19 @@ module.exports = {
       },
       keyframes: {
         "accordion-down": {
-          from: { height: 0 },
+          from: { height: "0" },
           to: { height: "var(--radix-accordion-content-height)" },
         },
         "accordion-up": {
           from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: 0 },
+          to: { height: "0" },
         },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out ",
-        "accordion-down-delay": "accordion-down 0.2s ease-out 1s",
-        "accordion-up-delay": "accordion-up 0.2s ease-out 1s"
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [
-    require("tailwindcss-animate"),
-    require("daisyui"),
-  ],
+  plugins: [require("tailwindcss-animate")],
 }
